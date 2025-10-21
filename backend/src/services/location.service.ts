426d5c8f32f1c3714d44dac82a662a38b740ca10
pham13 <thphph03@gmail.com>
2025-10-20 19:46:18 -0700

    location service

---
+++ backend/src/services/location.service.ts
@@ -0,0 +1,10 @@
+
+
+export class LocationService(location: [number, number]): Promise<[number, number]> {
+  // Location service methods would go here
+  // Call get user address in the same group
+  // Calculate the midpoint
+  // Get the travel time from each user to the midpoint
+  // Iterative improvement to minimize total travel time
+  // Return the optimal meeting location
+}
